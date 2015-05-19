5010f48633293d4c30408ec262d7bf963424e92c
Bruno Rocha <rochacbruno@gmail.com>
2015-05-19 12:48:48 -0300

    PEP8 :)

--- raise_if/__init__.py
+++ raise_if/__init__.py
@@ -1,4 +1,4 @@
-"""raise_if - Probably the small python package, only includes raise_if function"""
+"""raise_if - raise exception if condition"""
 
 __version__ = '0.1.0'
 __author__ = 'Bruno Rocha <rochacbruno@gmail.com>'
@@ -7,5 +7,8 @@ __all__ = ["raise_if"]
 
 def raise_if(condition, exception, *args, **kwargs):
     if condition:
-        raise exception if isinstance(exception, Exception) else exception(*args, **kwargs) 
-
+        raise exception if isinstance(
+            exception, Exception
+        ) else exception(
+            *args, **kwargs
+        )
