c5d066068632ed10867ed82e4de9fb5c36581a24
Bruno Rocha <rochacbruno@gmail.com>
2016-01-27 09:20:12 -0200

    Fix #1 added readme to MANIFEST

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="raise_if",
-    version="0.1.0",
+    version="0.1.2",
     url="https://github.com/rochacbruno/raise_if",
 
     author="Bruno Rocha",
